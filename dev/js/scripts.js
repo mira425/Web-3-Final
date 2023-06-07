a68baab17a5703161569b6efeda174287bbae9b1
mcc425 <130109031+mcc425@users.noreply.github.com>
2023-06-06 20:02:05 -0400

    class changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,9 +7,12 @@ burgerBtn.addEventListener("click", () => {
 
     if(!canSeeMenu){
         mainMenu.classList.add("show-menu");
+        burgerBtn.classList.add("open");
         canSeeMenu = true;
+      
     }else{
         mainMenu.classList.remove("show-menu");
+        burgerBtn.classList.remove("open");
         canSeeMenu = false;
     }
 })
